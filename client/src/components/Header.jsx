dc5667bfa46ba9524735014555e9d36d8bb04565
chomu <chomuhnaglimbu12@gmail.com>
2024-10-01 21:32:26 +0545

    complete sign up page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
           <Link to="/">
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
                 <span className='text-slate-500'>Fury</span>
-                <span className='text-slate-500'>Estate</span>
+                <span className='text-slate-600'>Estate</span>
           </h1>
           </Link>
   
